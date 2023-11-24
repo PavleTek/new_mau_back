1d8e75cf2e57730fd60222746fa3b7a2368c1883
tap3ah <tap3ahchina@gmail.com>
2023-11-24 16:27:05 +0100

    - PromiseAllInOrderDeferredPessimistic

--- tasks/ftp_transfer.js
+++ tasks/ftp_transfer.js
@@ -1,11 +1,10 @@
-import { readdir } from "fs/promises";
+import { readdir, rename } from "fs/promises";
 import Client from "ssh2-sftp-client";
 
 import { FTP_DIR_OUTBOX, FTP_DIR_SENT, FTP_FILE_NAME_TEMPLATE } from "../config.js";
 import { getFTPInfo } from "../model/samples.js";
-import { PromiseAllInOrderDeferredPessimistic } from "../utils.js";
 
-const ftpUpload = (fname, fpath) => {
+const ftpUpload = (fnames, fpath) => {
 	const sftp = new Client("example-client");
 	return getFTPInfo()
 		.then(({ ftp_host, ftp_username, ftp_password, ftp_remdir, ftp_port }) => sftp
@@ -16,18 +15,28 @@ const ftpUpload = (fname, fpath) => {
 				password: ftp_password
 			})
 //			.then(ftp_remdir => sftp.cwd(ftp_remdir))
-			.then(() => sftp
-				.put(`${fpath}${fname}`, `${ftp_remdir}/${fname}`)
+			.then(() => fnames
+				.reduce(
+					(acc, fname) => acc
+						.then(oldRes => sftp
+							.put(`${fpath}${fname}`, `${ftp_remdir}/${fname}`)
+							.then(newRes => rename(`${FTP_DIR_OUTBOX}${fname}`, `${FTP_DIR_SENT}${fname}`)
+								.then(() => ([ ...oldRes, newRes ]))
+							)
+							.catch(newRes => ([ ...oldRes, newRes ]))
+						),
+					Promise.resolve([])
+				)
 			)
 		)
-		.then(message => {
+		.then(messages => {
 			sftp.end();
-			return { success: true, fname, message };
+			return { success: true, fnames, message: messages.join("\n") };
 		})
 		.catch(e => {
 			if (!!sftp)
 				sftp.end();
-			return Promise.reject({ success: false, fname, message: e.message });
+			return Promise.reject({ success: false, fnames, message: e.message });
 		});
 };
 
@@ -37,13 +46,7 @@ export default ({ days, hours, minutes, seconds, milliseconds } = {}, ts_start =
 		.map(({ name }) => name)
 		.filter(name => FTP_FILE_NAME_TEMPLATE.test(name))
 	)
-	.then(list => list
-		.map(fname => () => ftpUpload(fname, FTP_DIR_OUTBOX)
-			.then(res => res.success ? rename(`${FTP_DIR_OUTBOX}${fname}`, `${FTP_DIR_SENT}${fname}`).then(() => res) : Promise.resolve(res))
-		)
+	.then(list => ftpUpload(list, FTP_DIR_OUTBOX)
 	)
-	.then(proms => PromiseAllInOrderDeferredPessimistic(proms)
-		.then(res => ({ success: true, message: JSON.stringify(res), ts_start }))
-		.catch(res => ({ success: false, message: JSON.stringify(res), ts_start }))
-	);
-
+	.then(res => ({ success: true, message: res, ts_start }))
+	.catch(res => ({ success: false, message: res, ts_start }));
