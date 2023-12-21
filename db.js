39bcdd542f6991679d322ca7641a4889dd97c0ba
tap3ah <tap3ahchina@gmail.com>
2023-12-21 11:34:44 +0100

    konverzija timestamp-a u string prilikom izvršavanja upita

--- db.js
+++ db.js
@@ -2,16 +2,19 @@ import mariadb from "mariadb";
 
 import { DB_AUTH } from "./config.js";
 
-export const query = (sql, params = []) => mariadb
-	.createConnection(DB_AUTH)
-	.then(conn => conn
-		.query(sql, params)
-		.catch(e => conn
-			.end()
-			.then(() => Promise.reject(e))
-		)
-		.then(res => conn
-			.end()
-			.then(() => Promise.resolve(res))
-		)
-	);
+export const query = (sql, params = []) => {
+	params = params.map(param => param instanceof Date ? param.toISOString().replace(/T/, " ").replace(/\.\d{3}Z$/, "") : param);
+	return mariadb
+		.createConnection(DB_AUTH)
+		.then(conn => conn
+			.query(sql, params)
+			.catch(e => conn
+				.end()
+				.then(() => Promise.reject(e))
+			)
+			.then(res => conn
+				.end()
+				.then(() => Promise.resolve(res))
+			)
+		);
+};
